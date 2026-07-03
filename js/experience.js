56a37bf00db633128dd65127dd8fa019c1433e82
hetotheha-sg <hetotheha@gmail.com>
2026-07-04 04:25:01 +0800

    Update experience.js

--- js/experience.js
+++ js/experience.js
@@ -1,28 +1,110 @@
 /*
 ==========================================================
 Singapore Explorer
-Phase 2
 Experience Engine
-Version 1.0
+Launch Sprint Edition
+Version 2.0.0
 ==========================================================
 */
 
 (function () {
 
-    "use strict";
+"use strict";
 
-    const Experience = {
+const Experience = {
 
-        version: "1.0.0",
+    version: "2.0.0",
 
-        init() {
-            console.log("Experience Engine v1.0 Loaded");
+    currentMoment: 0,
+
+    async begin() {
+
+        // Opening
+        if (window.Intro?.play) {
+            await Intro.play();
         }
 
-    };
+        // Opening Letter
+        if (window.Letter?.show) {
 
-    window.Experience = Experience;
+            await Letter.show({
 
-    Experience.init();
+                title: "Welcome.",
+
+                body:
+`Some places are visited.
+
+Others become part of who we are.
+
+Whether these streets are already familiar,
+or today marks a very first encounter,
+there are always stories waiting to be noticed.
+
+Perhaps a few are waiting for you today.`,
+
+                button: "Turn the Page"
+
+            });
+
+        }
+
+        // Initialise journey systems
+
+        Passport?.init?.();
+
+        Diary?.init?.();
+
+        Keepsakes?.init?.();
+
+        Journey?.init?.();
+
+        // Begin the first destination
+
+        this.goToArrival();
+
+    },
+
+    goToArrival() {
+
+        console.log("Arrival");
+
+        if (window.Game?.start) {
+
+            Game.start();
+
+            return;
+
+        }
+
+        console.log(
+            "Waiting for destination implementation..."
+        );
+
+    },
+
+    nextMoment() {
+
+        this.currentMoment++;
+
+        console.log(
+            "Next Moment:",
+            this.currentMoment
+        );
+
+    }
+
+};
+
+window.Experience = Experience;
+
+Experience.init = function () {
+
+    console.log(
+        "Experience Engine v2 Loaded"
+    );
+
+};
+
+Experience.init();
 
 })();
