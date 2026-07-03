854a87dc8d3c5347efbb47493293f6009e93e402
hetotheha-sg <hetotheha@gmail.com>
2026-07-03 22:51:22 +0800

    Phase 2: Add Experience Engine

---
+++ js/experience.js
@@ -0,0 +1,28 @@
+/*
+==========================================================
+Singapore Explorer
+Phase 2
+Experience Engine
+Version 1.0
+==========================================================
+*/
+
+(function () {
+
+    "use strict";
+
+    const Experience = {
+
+        version: "1.0.0",
+
+        init() {
+            console.log("Experience Engine v1.0 Loaded");
+        }
+
+    };
+
+    window.Experience = Experience;
+
+    Experience.init();
+
+})();
