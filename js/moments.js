5dfc41872737d938f37bc51beafa525c55d99337
hetotheha-sg <hetotheha@gmail.com>
2026-07-04 04:35:13 +0800

    Create moments.js

---
+++ js/moments.js
@@ -0,0 +1,43 @@
+(() => {
+
+"use strict";
+
+const Moments = {
+
+    async arrival() {
+
+    },
+
+    async jewel() {
+
+    },
+
+    async kampong() {
+
+    },
+
+    async chinatown() {
+
+    },
+
+    async littleIndia() {
+
+    },
+
+    async hawker() {
+
+    },
+
+    async sentosa() {
+
+    },
+
+    async untilWeMeetAgain() {
+
+    }
+
+};
+
+window.Moments = Moments;
+
+})();
